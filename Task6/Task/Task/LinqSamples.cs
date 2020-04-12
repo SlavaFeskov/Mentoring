3fc7778a734620d25ab6a4833751aa8b468b7b24
Viachaslau Fiaskou <viachaslau.fiaskou.ext@hoffmann-group.com>
2020-04-12 19:15:44 +0300

    Fixes after review 1

--- Task6/Task/Task/LinqSamples.cs
+++ Task6/Task/Task/LinqSamples.cs
@@ -24,13 +24,21 @@ namespace Task
 
         [Category("Task6")]
         [Title("Task 6 - 1")]
-        [Description("Returns a list of customers, which have their overall orders sum above than some predefined value.")]
+        [Description(
+            "Returns a list of customers, which have their overall orders sum above than some predefined value.")]
         public void Linq1()
         {
             var totalSums = new List<decimal> { 50000, 100000 };
+            var customersTotalRevenues = this.dataSource.Customers.Select(
+                c => new
+                {
+                    CustomerName = c.CompanyName,
+                    TotalRevenue = c.Orders.Sum(o => o.Total)
+                }).ToList();
+
             foreach (var totalSum in totalSums)
             {
-                var customers = this.dataSource.Customers.Where(c => c.Orders.Sum(o => o.Total) > totalSum);
+                var customers = customersTotalRevenues.Where(c => c.TotalRevenue > totalSum);
                 ObjectDumper.Write(customers);
                 ObjectDumper.Write("\r\n");
             }
@@ -112,7 +120,8 @@ namespace Task
 
         [Category("Task6")]
         [Title("Task 6 - 5")]
-        [Description("Returns a list of customers with their first order date ordered descending by year, then descending by month and then descending by orders count for that customer.")]
+        [Description(
+            "Returns a list of customers with their first order date ordered descending by year, then descending by month and then descending by orders count for that customer.")]
         public void Linq5()
         {
             var customers = this.GetCustomersWithStartDate().Where(c => c.StartDate.HasValue)
@@ -123,7 +132,8 @@ namespace Task
 
         [Category("Task6")]
         [Title("Task 6 - 6")]
-        [Description("Returns a list of customers which have not numeric PostalCode or don't have Region or don't have phone operator (no brackets in phone number).")]
+        [Description(
+            "Returns a list of customers which have not numeric PostalCode or don't have Region or don't have phone operator (no brackets in phone number).")]
         public void Linq6()
         {
             var postalCodeRegex = new Regex(@"\d+");
@@ -137,7 +147,8 @@ namespace Task
 
         [Category("Task6")]
         [Title("Task 6 - 7")]
-        [Description("Groups products by categories, then in each group groups by presence in stock and orders by price in the last group.")]
+        [Description(
+            "Groups products by categories, then in each group groups by presence in stock and orders by price in the last group.")]
         public void Linq7()
         {
             var products = this.dataSource.Products.GroupBy(p => p.Category).Select(
@@ -157,7 +168,8 @@ namespace Task
 
         [Category("Task6")]
         [Title("Task 6 - 8")]
-        [Description("Groups products by UnitPrice (Cheap, Medium, Expensive), the borders for each price group are predefined.")]
+        [Description(
+            "Groups products by UnitPrice (Cheap, Medium, Expensive), the borders for each price group are predefined.")]
         public void Linq8()
         {
             var calcPriceGroup = new Func<Product, PriceGroup>(
@@ -189,14 +201,17 @@ namespace Task
 
         [Category("Task6")]
         [Title("Task 6 - 9")]
-        [Description("Returns list of cities with their average orders overall sum and average intensity of orders for each customer in the city.")]
+        [Description(
+            "Returns list of cities with their average orders overall sum and average intensity of orders for each customer in the city.")]
         public void Linq9()
         {
             var result = this.dataSource.Customers.GroupBy(c => c.City).Select(
                 g => new
                 {
-                    City = g.Key, AvgIncome = Math.Round(g.Select(c => c.Orders.Sum(o => o.Total)).Average(), 2),
-                    AvgIntensity = Math.Round(g.Select(c => c.Orders.Length).Average(), 2), CustomersCount = g.Count()
+                    City = g.Key,
+                    AvgIncome = Math.Round(g.Select(c => c.Orders.Sum(o => o.Total)).Average(), 2),
+                    AvgIntensity = Math.Round(g.Select(c => c.Orders.Length).Average(), 2),
+                    NumberOfCustomers = g.Count()
                 });
             ObjectDumper.Write(result);
         }
@@ -208,8 +223,9 @@ namespace Task
         {
             var result1 = this.dataSource.Customers.SelectMany(
                 c => c.Orders.GroupBy(
-                    o => o.OrderDate.Year,
-                    (y, o) => new { Customer = c.CompanyName, Year = y, OrdersCount = o.Count() }).OrderBy(o => o.Year));
+                        o => o.OrderDate.Year,
+                        (y, o) => new { Customer = c.CompanyName, Year = y, OrdersCount = o.Count() })
+                    .OrderBy(o => o.Year));
 
             var result2 = this.dataSource.Customers.SelectMany(
                 c => c.Orders.GroupBy(
